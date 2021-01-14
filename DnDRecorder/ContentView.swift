de4439d4cb83a62c568ab748a8689cdf560c46e0
Samuel Chen <samuel-chen@foxmail.com>
2021-01-14 16:58:08 +1300

    Testing adding text and committing

---
+++ DnDRecorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DnDRecorder
+//
+//  Created by Samuel Chen on 14/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! Here")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DnDRecorder/DnDRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDRecorderApp.swift
+//  DnDRecorder
+//
+//  Created by Samuel Chen on 14/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DnDRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
